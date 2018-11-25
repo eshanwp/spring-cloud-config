d126b34a90f43fd56a519a981950fefc63559b4c
eshan <eshanwp@gmail.com>
2018-11-25 23:40:25 +0530

    change bootstrap.properties

--- config-client/src/main/java/com/cloud/config/client/controller/HelloController.java
+++ config-client/src/main/java/com/cloud/config/client/controller/HelloController.java
@@ -1,15 +1,17 @@
 package com.cloud.config.client.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequestMapping("/api")
 public class HelloController {
 
-    @Value("${welcome.message}")
+    @Value("${message}")
     String welcomeText;
 
     @GetMapping(value = "/welcome")
